7a297a155cb39a23a1daba8f0cb37999495c49fb
iChenwin <iChenwin@gmail.com>
2019-04-25 16:40:53 +0000

    使用swscale将视频解码得的YUV转码为RGB24

--- 00yuv2rgb/yuv2rgb.c
+++ 00yuv2rgb/yuv2rgb.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
 
 void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame);
 
 int main(int argc, char *argv[])
 {
+    // 获取AVFormatContext句柄
     AVFormatContext *pFormatCtx = avformat_alloc_context();
 
-    // open video file
+    // 打开一个流媒体文件 open video file
     if (avformat_open_input(&pFormatCtx, argv[1], NULL, NULL)) {
         fprintf(stderr, "open input failed\n");
         return -1;
     }
 
-    // retrieve stream information
+    // 获取多媒体信息，存至句柄 retrieve stream information
     if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
         fprintf(stderr, "find stream info failed\n");
         return -1;
     }
 
-    // dump information about file onto standard error
+    // 打印多媒体信息 dump information about file onto standard error
     av_dump_format(pFormatCtx, 0, argv[1], 0);
     
-    // find the video stream
+    // 通过句柄，找出视频流 find the video stream
     int videoStream = -1;
     videoStream = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
     if (-1 == videoStream) {
@@ -34,7 +37,7 @@ int main(int argc, char *argv[])
     AVCodecParameters *pCodecPar = NULL;
     pCodecPar = pFormatCtx->streams[videoStream]->codecpar;
     
-    // find the decoder for the video stream
+    // 搜索合适的视频解码器 find the decoder for the video stream
     AVCodec *pCodec = NULL;
     pCodec = avcodec_find_decoder(pCodecPar->codec_id);
     if (NULL == pCodec) {
@@ -47,7 +50,7 @@ int main(int argc, char *argv[])
     // 因为 AVStream::codec 被弃用，AVCodecContext 需要通过 AVCodecParameters 转换得到
     avcodec_parameters_to_context(pCodecCtx, pCodecPar);
     
-    //open Codec
+    // 打开视频解码器 open Codec
     if (avcodec_open2(pCodecCtx, pCodec, NULL)) {
         fprintf(stderr, "Codec open failed!\n");
         return -1;
@@ -55,7 +58,7 @@ int main(int argc, char *argv[])
     
     AVFrame *pFrame = NULL;
     AVFrame *pFrameRGB = NULL;
-    // Allocate video frame
+    // 分配两个视频帧，pFrame保存原始帧，pFrameRGB存放转换后的RGB帧 Allocate video frame
     pFrame = av_frame_alloc();
     pFrameRGB = av_frame_alloc();
     if (NULL == pFrameRGB || NULL == pFrame) {
@@ -64,18 +67,19 @@ int main(int argc, char *argv[])
     }
     uint8_t *buffer = NULL;
     int numBytes = 0;
-    // Determine required buffer size and allocate buffer
-    printf("w:%d, h:%d\n", pCodecCtx->width, pCodecCtx->height);
+    // 计算解码后原始数据所需缓冲区大小，并分配内存空间 Determine required buffer size and allocate buffer
     numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 1);
     buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
     
     av_image_fill_arrays(pFrameRGB->data, pFrameRGB->linesize, buffer, AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 1);
-    
-    printf("buffer size:%d\n", numBytes);
-    
+        
     int frameFinished = 0;
     AVPacket packet = {0};
     int i = 0;
+    struct SwsContext *img_convert_ctx = NULL;
+    // 获取swscale句柄
+    img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,  
+        pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BICUBIC, NULL, NULL, NULL); 
     while (av_read_frame(pFormatCtx, &packet) >= 0) {
         // Is this a packet from the video stream?
         if (packet.stream_index == videoStream) {
@@ -85,11 +89,14 @@ int main(int argc, char *argv[])
                 continue;
             while (avcodec_receive_frame(pCodecCtx, pFrame) == 0) {
                 // Convert the image from its native format to RGB
-                img_convert((AVPicture *)pFrameRGB, AV_PIX_FMT_RGB24, (AVPicture *)pFrame, 
-                            pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+                //img_convert((AVPicture *)pFrameRGB, AV_PIX_FMT_RGB24, (AVPicture *)pFrame, 
+                //            pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+                sws_scale(img_convert_ctx, (const unsigned char* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
                 // Save the frame to disk
                 if (++i <= 5)
                     SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, i);
+                else
+                    break;
             }
 #if 0            
             avcodec_decode_video(pCodecCtx, pFrame, &frameFinished, packet.data, packet.size);
@@ -117,10 +124,10 @@ int main(int argc, char *argv[])
     av_free(pFrame);
     
     // Close the codec
-    avcodec_colse(pCodecCtx);
+    avcodec_close(pCodecCtx);
     
     // Close the video file
-    av_close_input_file(pFormatCtx);
+    avformat_close_input(&pFormatCtx);
        
     return 0;
 }
